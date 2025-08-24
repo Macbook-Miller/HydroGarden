a45f5ee59fa7a012db377787f404d93926e2a9ee
Macbook-Miller <frederikvaleur@icloud.com>
2025-08-24 20:26:36 +0200

    Initial Commit

---
+++ HydroGarden/HydroGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  HydroGardenApp.swift
+//  HydroGarden
+//
+//  Created by Fred on 24/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct HydroGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
